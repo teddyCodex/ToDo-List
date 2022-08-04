701ac11d29936041129d3a78e5877bfa234f2711
Teddy <samted.uche@gmail.com>
2022-08-04 09:03:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+const inputEl = document.getElementById("input");
+const toDoList = document.getElementById("todo-list");
+const add = document.getElementById("add");
+const clearComplete = document.getElementById("clear-complete");
+const empty = document.getElementById("empty");
+const save = document.getElementById("save");
+let listBag = [];
+
+function render(itemText, completed) {
+  let listItem = document.createElement("li");
+  let listText = document.createTextNode(itemText);
+  listItem.appendChild(listText);
+  if (completed) {
+    listItem.classList.add("completed");
+  }
+  toDoList.appendChild(listItem);
+  listItem.addEventListener("dblclick", toggleCompleteState);
+}
+
+// Adding Items to the list
+add.addEventListener("click", function () {
+  let itemText = inputEl.value;
+  if (inputEl.value !== "") {
+    listBag.push(itemText);
+    inputEl.value = "";
+    render(itemText, false);
+  }
+  console.log(listBag);
+});
+
+// Mark Items as complete
+function toggleCompleteState() {
+  if (this.classList.contains("completed")) {
+    this.classList.remove("completed");
+  } else {
+    this.classList.add("completed");
+  }
+}
+
+// Delete Completed Items
+function clearCompleted() {
+  let completedItems = toDoList.getElementsByClassName("completed");
+  while (completedItems.length > 0) {
+    completedItems.item(0).remove();
+  }
+}
+
+clearComplete.addEventListener("click", function () {
+  clearCompleted();
+});
+
+// Empty the list
+empty.addEventListener("click", function () {
+  listBag = [];
+  toDoList.innerHTML = "";
+  localStorage.clear();
+});
+
+// Save the list
+function saveList() {
+  localStorage.setItem("toDos", JSON.stringify(listBag));
+}
+
+save.addEventListener("click", function () {
+  saveList();
+});
