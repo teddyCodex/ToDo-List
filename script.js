b6acc99c5a2ba8c9ce5c614f20ae2d5dae67c0df
Teddy <samted.uche@gmail.com>
2022-08-04 14:15:28 +0100

    newly commit

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ empty.addEventListener("click", function () {
 function saveList() {
   let inComplete = Array.from(toDoList.getElementsByClassName("unCompleted"))
   for (i = 0; i < inComplete.length; i++) {
-    localStorage.setItem("toDos", JSON.stringify(inComplete[i]))
+    localStorage.setItem("toDos", inComplete[i])
     console.log(inComplete[i]);
   }
 }
